5c9a21c4088c4ba8634b8c4178617612da1d10a5
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-10-01 21:10:05 -0300

    Improved zerosFunc

--- zeroRoots.hs
+++ zeroRoots.hs
@@ -1,34 +1,47 @@
-fn x = x - (x^5 - (10/9)*x^3 + (5/21)*x)/(5*(x^4) - 10*(x^2)/3 + 5/21)
-f x = (x-(sqrt $ 64/(900/x^2-1)))^2 +64-(400/x^2)*((x-(sqrt $ 64/(900/x^2-1)))^2)
+xi = 1.5          -- X inicial
+err = 0.00001     -- Erro predefinido
+f x = x^2 + x - 6 -- Função F(x)=0
+
+-- Função iterativa do método de newton
+fn x = x - (x^2 + x - 6)/(2*x + 1)
+-- Função falsa posição
 ffp a b = (a*(f b) - b*(f a))/((f b) - (f a))
-fix x = (x^3)/3
+-- Função iterativa do ponto fixo
+fix x = sqrt(6 - x)
+-- Funcao para metodo da secante
 fs k k1 = k1 - (f k1)*(k1 - k)/((f k1) - (f k))
 
-newton e x0 = newton' e x0 $ (abs $ x0)+e+1
-newton' e xk1 xk
-  | ((abs $ xk - xk1) < e) = []
-  | otherwise = xk2:newton' e xk2 xk1
+-- Critérios de parada
+relative e xk xk1 = (abs xk1)*e > (abs $ xk1 - xk)
+absolute e xk xk1 = e > (abs $ xk1 - xk)
+residual e f xk = e > abs (f xk)
+
+-- Chamadas para aplicação dos métodos:
+newton e x0 = newton' e x0 (fn x0)
+newton' e xk xk1
+  | (residual e (f) xk) = []
+  | otherwise = xk:newton' e xk1 xk2
   where xk2 = fn xk1
 
 bissect e [a,b]
-  | ((abs $ a-b) < e) = []
+  | (absolute e a b) = []
   | otherwise = cut:bissect e i
   where i = if (f cut)*(f a)<0 then [a,cut] else [cut,b]
         cut = (a+b)/2
 
 falsePosition e [a,b]
-  | ((abs $ a-b) < e) = []
+  | (absolute e a b) = []
   | otherwise = cut:bissect e i
   where i = if (f cut)*(f a)<0 then [a,cut] else [cut,b]
         cut = ffp a b
 
-fixatedPoint e x0 = fixatedPoint' e x0 $ (abs $ x0)+e+1
-fixatedPoint' e xk1 xk
-  | ((abs $ xk - xk1) < e) = []
-  | otherwise = xk2:fixatedPoint' e xk2 xk1
+fixedPoint e x0 = fixedPoint' e x0 $ fix x0
+fixedPoint' e xk xk1
+  | (absolute e xk xk1) = []
+  | otherwise = xk2:fixedPoint' e xk1 xk2
   where xk2 = fix xk1
 
-sec e k k1
-  | ((abs $ k - k1) < e) = []
-  | otherwise = k1:sec e k1 k2
-  where k2 = fs k k1
+sec e xk xk1
+  | (absolute e xk xk1) = []
+  | otherwise = xk1:sec e xk1 xk2
+  where xk2 = fs xk xk1
